70d0f6e082072873ff573f49ab25b0b75ffbf636
Kyle Johnson <kyledjohnson@hotmail.com>
2021-05-27 16:16:02 -0500

    Added Route functionality for the landing and About pages

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import {useState, useEffect} from 'react'
+import {BrowserRouter as Router, Route } from 'react-router-dom'
 import Header from './components/Header'
 import Footer from './components/Footer'
 import Tasks from './components/Tasks'
 import AddTask from './components/AddTask'
+import About from './components/About'
 
 
 const App = () => {
@@ -90,15 +92,22 @@ const App = () => {
 
   // && is shorthand for ternary operator with no else needed
   return (
+    <Router>
     <div className="container">
       <Header onAdd={() => setShowAddTask(!showAddTask)} showAdd={showAddTask}/>
-      {showAddTask && <AddTask onAdd={addTask}/>}
-      { tasks.length > 0
-        ? <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder}/>
-        : 'No Tasks To Show'
-      }
+      <Route path='/' exact render={(props) => (
+        <>
+          {showAddTask && <AddTask onAdd={addTask}/>}
+            { tasks.length > 0
+              ? <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder}/>
+              : 'No Tasks To Show'
+            }
+        </>
+      ) } />
+      <Route path='/about' component={About}/>
       <Footer/>
     </div>
+    </Router>
   );
 }
 
