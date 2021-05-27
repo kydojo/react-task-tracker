5473afbca89a1172f174d9e2df4a11f17435c00b
Kyle Johnson <kyledjohnson@hotmail.com>
2021-05-27 15:55:56 -0500

    Added persistent PUT functionality for the reminder state

--- src/App.js
+++ src/App.js
@@ -68,11 +68,23 @@ const App = () => {
   }
 
   // Toggle Reminder
-  const toggleReminder = (id) => {
+  const toggleReminder = async (id) => {
+    const taskToToggle = await fetchTask(id)
+    const updatedTask = {...taskToToggle, reminder: !taskToToggle.reminder}
+
+    const res = await fetch(`http://localhost:5000/tasks/${id}`, {
+      method: 'PUT',
+      headers: {
+        'Content-type': 'application/json'
+      },
+      body: JSON.stringify(updatedTask)
+    })
+
+    const data = await res.json()
+
     // iterate over tasks and only toggle the specified task (by id)
     setTasks(tasks.map((task) => task.id === id
-              ? {...task, reminder: !task.reminder}
-              : task))
+              ? {...task, reminder: !task.reminder} : task))
   }
 
   // && is shorthand for ternary operator with no else needed
