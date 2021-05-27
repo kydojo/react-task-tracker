9b68a3e4f4e709fac17588ced67abfe4680aa7d9
Kyle Johnson <kyledjohnson@hotmail.com>
2021-05-27 15:32:43 -0500

    Refactored useEffect to set tasks with tasks from json-server via setTasks

--- src/App.js
+++ src/App.js
@@ -16,14 +16,19 @@ const App = () => {
   const [showAddTask, setShowAddTask] = useState(false)
 
   useEffect(() => {
-    const fetchTasks = async () => {
-      const res = await fetch('http://localhost:5000/tasks')
-      const data = await res.json()
-      console.log(data)
+    const getTasks = async () => {
+      const tasksFromServer = await fetchTasks()
+      setTasks(tasksFromServer)
     }
-    fetchTasks()
+    getTasks()
   }, [])
 
+  // Fetch Tasks
+  const fetchTasks = async () => {
+    const res = await fetch('http://localhost:5000/tasks')
+    const data = await res.json()
+    return data
+  }
   // Add Task
   const addTask = (task) => {
     // generate random task id
